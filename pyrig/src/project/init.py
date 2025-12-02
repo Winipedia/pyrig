3986f9987471b261e47eb2cb09ab51923f1dae91
Winipedia <win.steveker@gmx.de>
2025-12-02 12:12:50 +0000

    fixed fixture and cron

--- pyrig/src/project/init.py
+++ pyrig/src/project/init.py
@@ -11,18 +11,34 @@ from pyrig.dev.configs.base.base import ConfigFile
 from pyrig.dev.configs.git.pre_commit import PreCommitConfigConfigFile
 from pyrig.dev.configs.pyproject import PyprojectConfigFile
 from pyrig.dev.configs.testing.conftest import ConftestConfigFile
-from pyrig.src.project.create_root import create_root
-from pyrig.src.testing.create_tests import create_tests
+from pyrig.src.os.os import run_subprocess
+from pyrig.src.project.mgt import (
+    get_project_mgt_run_pyrig_cli_cmd_args,
+)
 
 logger = logging.getLogger(__name__)
+
+
+def run_create_root() -> None:
+    """Run create root."""
+    from pyrig.dev.cli.subcommands import create_root  # noqa: PLC0415
+
+    run_subprocess(get_project_mgt_run_pyrig_cli_cmd_args(create_root))
+
+
+def run_create_tests() -> None:
+    """Run create tests."""
+    from pyrig.dev.cli.subcommands import create_tests  # noqa: PLC0415
+
+    run_subprocess(get_project_mgt_run_pyrig_cli_cmd_args(create_tests))
 
 
 SETUP_STEPS: list[Callable[..., Any]] = [
     ConfigFile.init_priority_config_files,  # write dev deps to pyproject.toml
     PyprojectConfigFile.install_dependencies,  # to install dev deps
     PyprojectConfigFile.update_dependencies,  # to update dev deps
-    create_root,
-    create_tests,
+    run_create_root,
+    run_create_tests,
     PreCommitConfigConfigFile.run_hooks,
     ConftestConfigFile.run_tests,
     PyprojectConfigFile.install_dependencies,  # to activate cli
