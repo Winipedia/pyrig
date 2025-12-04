77fe6b890079c01acfa73e159d1d1a6ef8f470ef
Winipedia <win.steveker@gmx.de>
2025-12-04 16:39:14 +0000

    resource init config fix

--- pyrig/dev/configs/python/resources_init.py
+++ pyrig/dev/configs/python/resources_init.py
@@ -1,29 +1,15 @@
 """Config utilities for resources/__init__.py."""
 
-from pathlib import Path
 from types import ModuleType
 
 from pyrig.dev.artifacts import resources
-from pyrig.dev.configs.base.base import CopyModuleOnlyDocstringConfigFile
-from pyrig.src.modules.module import get_isolated_obj_name
+from pyrig.dev.configs.base.base import InitConfigFile
 
 
-class ResourcesInitConfigFile(CopyModuleOnlyDocstringConfigFile):
+class ResourcesInitConfigFile(InitConfigFile):
     """Config file for resources/__init__.py."""
 
     @classmethod
     def get_src_module(cls) -> ModuleType:
         """Get the source module."""
         return resources
-
-    @classmethod
-    def get_parent_path(cls) -> Path:
-        """Get the path to the config file."""
-        path = super().get_parent_path()
-        # this path will be parent of resources
-        return path / get_isolated_obj_name(resources)
-
-    @classmethod
-    def get_filename(cls) -> str:
-        """Get the filename of the config file."""
-        return "__init__"
