023639b597057a3458c237fa21bbf6cf2ec6ce0c
Winipedia <winipedia@gmx.de>
2025-12-10 02:19:14 +0000

    update

--- pyrig/dev/configs/testing/main_test.py
+++ pyrig/dev/configs/testing/main_test.py
@@ -59,7 +59,6 @@ class MainTestConfigFile(PythonPackageConfigFile):
 
 def test_main(main_test_fixture: None) -> None:
     """Test func for main."""
-    # can be called via cli
     assert main_test_fixture is None
 '''
 
