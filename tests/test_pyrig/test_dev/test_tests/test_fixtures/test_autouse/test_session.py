cf8bd70bd5fd53dba22a4e77a8320fd00af6e690
Winipedia <winipedia@gmx.de>
2026-01-21 12:19:59 +0000

    test fixes

--- tests/test_pyrig/test_dev/test_tests/test_fixtures/test_autouse/test_session.py
+++ tests/test_pyrig/test_dev/test_tests/test_fixtures/test_autouse/test_session.py
@@ -1,72 +1,49 @@
-"""module for the following module path (maybe truncated).
+"""test module.
 
-tests.test_pyrig.test_testing.test_tests.test_base.test_scopes.test_session
+We do not test the autouse fixtures directly,
+as it is not easily feasible to basically test a test.
 """
 
-from pyrig.dev.utils.testing import skip_fixture_test
 
-
-@skip_fixture_test
 def test_assert_root_is_correct() -> None:
     """Test func for assert_config_files_are_correct."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_no_namespace_packages() -> None:
     """Test func for assert_no_namespace_packages."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_all_src_code_in_one_package() -> None:
     """Test func for assert_all_src_code_in_one_package."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_src_package_correctly_named() -> None:
     """Test func for assert_src_package_correctly_named."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_all_modules_tested() -> None:
     """Test func for assert_all_modules_tested."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_no_unit_test_package_usage() -> None:
     """Test func for assert_no_unit_test_package_usage."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_dependencies_are_up_to_date() -> None:
     """Test function."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_src_runs_without_dev_deps() -> None:
     """Test function."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_src_does_not_use_dev() -> None:
     """Test function."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_no_unstaged_changes() -> None:
     """Test function."""
-    raise NotImplementedError
 
 
-@skip_fixture_test
 def test_assert_project_mgt_is_up_to_date() -> None:
     """Test function."""
-    raise NotImplementedError
