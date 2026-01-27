1ac7151068dae585118c7b045dc5dbe1913483df
Winipedia <winipedia@gmx.de>
2026-01-27 12:21:02 +0000

    simlify url tests assertions

--- tests/test_pyrig/test_dev/test_utils/test_urls.py
+++ tests/test_pyrig/test_dev/test_utils/test_urls.py
@@ -11,18 +11,15 @@ def test_get_codecov_url() -> None:
     """Test function."""
     url = get_codecov_url()
     assert isinstance(url, str), f"Expected url to be str, got {type(url)}"
-    assert "codecov.io" in url, f"Expected 'codecov.io' in url, got {url}"
 
 
 def test_get_pypi_url() -> None:
     """Test function."""
     url = get_pypi_url()
     assert isinstance(url, str), f"Expected url to be str, got {type(url)}"
-    assert "pypi.org" in url, f"Expected 'pypi.org' in url, got {url}"
 
 
 def test_get_pypi_badge_url() -> None:
     """Test function."""
     url = get_pypi_badge_url()
     assert isinstance(url, str), f"Expected url to be str, got {type(url)}"
-    assert "img.shields.io" in url, f"Expected 'img.shields.io' in url, got {url}"
