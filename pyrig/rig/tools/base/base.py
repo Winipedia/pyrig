d617996b59cb5366058f311597c8ddf6ea27db56
Winipedia <winipedia@gmx.de>
2026-02-21 15:16:35 +0000

    removed unnecessary caches and singleton

--- pyrig/rig/tools/base/base.py
+++ pyrig/rig/tools/base/base.py
@@ -32,7 +32,7 @@ from typing import Self
 from pyrig.rig import tools
 from pyrig.src.processes import Args
 from pyrig.src.string_ import make_linked_badge_markdown
-from pyrig.src.subclass import SingletonDependencySubclass
+from pyrig.src.subclass import DependencySubclass
 
 logger = logging.getLogger(__name__)
 
@@ -49,7 +49,7 @@ class ToolGroup:
     TESTING = "testing"
 
 
-class Tool(SingletonDependencySubclass):
+class Tool(DependencySubclass):
     """Abstract base for tool command argument construction.
 
     Provides consistent interface for constructing command-line arguments.
