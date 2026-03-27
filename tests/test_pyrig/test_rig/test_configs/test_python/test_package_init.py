d05ce5903130507daab731092cf2f0278e2c84e5
Winipedia <winipedia@gmx.de>
2026-03-27 04:54:31 +0000

    removed src package abstraction

---
+++ tests/test_pyrig/test_rig/test_configs/test_python/test_package_init.py
@@ -0,0 +1,47 @@
+"""module."""
+
+from contextlib import chdir
+from pathlib import Path
+
+from pytest_mock import MockerFixture
+
+import pyrig
+from pyrig.rig.configs.python.package_init import PackageInitConfigFile
+
+
+class TestPackageInitConfigFile:
+    """Test class."""
+
+    def test_parent_path(self) -> None:
+        """Test method."""
+        assert PackageInitConfigFile.I.parent_path() == Path(pyrig.__name__)
+
+    def test_create_file(self, mocker: MockerFixture) -> None:
+        """Test method."""
+        spy = mocker.spy(
+            PackageInitConfigFile,
+            PackageInitConfigFile.I.delete_root_main.__name__,
+        )
+        PackageInitConfigFile.I.create_file()
+        spy.assert_called_once()
+
+    def test_delete_root_main(self, tmp_path: Path) -> None:
+        """Test method."""
+        with chdir(tmp_path):
+            # Create a dummy main.py file
+            main_file = tmp_path / "main.py"
+            main_file.write_text("print('Hello, World!')")
+
+            # Ensure the file exists
+            assert main_file.exists(), "main.py should exist before deletion"
+
+            # Call the delete_root_main method
+            PackageInitConfigFile.I.delete_root_main()
+
+            # Check that the file has been deleted
+            assert not main_file.exists(), "main.py should be deleted"
+
+    def test_src_module(self) -> None:
+        """Test method."""
+        module = PackageInitConfigFile.I.src_module()
+        assert module == pyrig
