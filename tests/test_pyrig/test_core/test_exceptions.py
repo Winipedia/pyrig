afd85d473bcbb341d43b610548a3fcc02437676e
Winipedia <winipedia@gmx.de>
2026-04-09 00:18:37 +0100

    added base error

---
+++ tests/test_pyrig/test_core/test_exceptions.py
@@ -0,0 +1,85 @@
+"""Test module."""
+
+
+class TestPyrigError:
+    """Test class."""
+
+
+class TestPyrigDiscoveryError:
+    """Test class."""
+
+
+class TestPyrigSubclassResolutionError:
+    """Test class."""
+
+
+class TestPyrigGraphError:
+    """Test class."""
+
+
+class TestPyrigGraphCycleError:
+    """Test class."""
+
+
+class TestPyrigImportError:
+    """Test class."""
+
+
+class TestPyrigModuleImportError:
+    """Test class."""
+
+
+class TestPyrigModulePathError:
+    """Test class."""
+
+
+class TestPyrigInspectionError:
+    """Test class."""
+
+
+class TestPyrigSecurityError:
+    """Test class."""
+
+
+class TestPyrigConfigurationError:
+    """Test class."""
+
+
+class TestPyrigConfigValidationError:
+    """Test class."""
+
+
+class TestPyrigConfigSerializationError:
+    """Test class."""
+
+
+class TestPyrigConfigDefinitionError:
+    """Test class."""
+
+
+class TestPyrigSecretManagementError:
+    """Test class."""
+
+
+class TestPyrigCredentialError:
+    """Test class."""
+
+
+class TestPyrigWorkflowDefinitionError:
+    """Test class."""
+
+
+class TestPyrigLicenseDetectionError:
+    """Test class."""
+
+
+class TestPyrigVersionConstraintError:
+    """Test class."""
+
+
+class TestPyrigTestingInfrastructureError:
+    """Test class."""
+
+
+class TestPyrigMirrorTestError:
+    """Test class."""
