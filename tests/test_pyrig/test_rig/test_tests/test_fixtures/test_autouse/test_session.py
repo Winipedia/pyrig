11e8150965ba5faa02ed27cf818ac923263ce3f0
Winipedia <winipedia@gmx.de>
2026-04-11 12:45:41 +0100

    better err msg

--- tests/test_pyrig/test_rig/test_tests/test_fixtures/test_autouse/test_session.py
+++ tests/test_pyrig/test_rig/test_tests/test_fixtures/test_autouse/test_session.py
@@ -3,31 +3,3 @@
 We do not test the autouse fixtures directly.
 They are tests themselves and are hard to test due to being session scoped and autouse.
 """
-
-
-def test_assert_root_is_correct() -> None:
-    """Test function."""
-
-
-def test_assert_no_namespace_packages() -> None:
-    """Test function."""
-
-
-def test_assert_all_modules_tested() -> None:
-    """Test function."""
-
-
-def test_assert_dependencies_are_up_to_date() -> None:
-    """Test function."""
-
-
-def test_assert_src_runs_without_dev_deps() -> None:
-    """Test function."""
-
-
-def test_assert_no_unstaged_changes() -> None:
-    """Test function."""
-
-
-def test_assert_package_manager_is_up_to_date() -> None:
-    """Test function."""
