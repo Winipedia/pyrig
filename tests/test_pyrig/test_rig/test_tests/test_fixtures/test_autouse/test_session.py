7ae8a128183459c322c28c8329f160e12e14ddbc
Winipedia <winipedia@gmx.de>
2026-04-20 23:48:25 +0100

    expanded all tests

--- tests/test_pyrig/test_rig/test_tests/test_fixtures/test_autouse/test_session.py
+++ tests/test_pyrig/test_rig/test_tests/test_fixtures/test_autouse/test_session.py
@@ -8,14 +8,21 @@ import pytest
 from pytest_mock import MockerFixture
 
 from pyrig.core.introspection.inspection import unwrapped_obj
+from pyrig.core.requests import internet_is_available
 from pyrig.rig.configs.base.config_file import ConfigFile
 from pyrig.rig.configs.markdown.readme import ReadmeConfigFile
 from pyrig.rig.tests import fixtures
+from pyrig.rig.tests.fixtures.autouse import session
 from pyrig.rig.tests.fixtures.autouse.session import (
     all_config_files_correct,
+    all_dependencies_updated,
+    all_modules_tested,
+    no_dev_deps_in_source_code,
     no_namespace_packages,
     no_unstaged_changes_in_ci,
+    package_manager_updated,
 )
+from pyrig.rig.tests.mirror_test import MirrorTestConfigFile
 from pyrig.rig.tools.remote_version_controller import RemoteVersionController
 from pyrig.rig.tools.version_controller import VersionController
 from pyrig.rig.utils.packages import find_namespace_packages
@@ -82,15 +89,70 @@ def test_no_namespace_packages(mocker: MockerFixture) -> None:
 
 def test_all_modules_tested(mocker: MockerFixture) -> None:
     """Test function."""
+    unwrapped_func = unwrapped_obj(all_modules_tested)
+
+    incorrect_mock = mocker.patch.object(
+        MirrorTestConfigFile,
+        MirrorTestConfigFile.incorrect_subclasses.__name__,
+        return_value=[MirrorTestConfigFile.generate_subclass(session)],
+    )
+    with pytest.raises(AssertionError, match=r"Found incorrect test modules."):
+        unwrapped_func()
+
+    incorrect_mock.assert_called_once()
 
 
-def test_all_dependencies_updated() -> None:
+def test_all_dependencies_updated(
+    mocker: MockerFixture, standard_output_error_template: str
+) -> None:
     """Test function."""
+    unwrapped_func = unwrapped_obj(all_dependencies_updated)
+    internet_available_mock = mocker.patch(
+        all_dependencies_updated.__module__ + "." + internet_is_available.__name__,
+        return_value=not internet_is_available(),
+    )
+
+    unwrapped_func(standard_output_error_template=standard_output_error_template)
+
+    internet_available_mock.assert_called_once()
 
 
-def test_no_dev_deps_in_source_code() -> None:
+def test_no_dev_deps_in_source_code(
+    mocker: MockerFixture,
+    tmp_path_factory: pytest.TempPathFactory,
+    standard_output_error_template: str,
+) -> None:
     """Test function."""
+    unwrapped_func = unwrapped_obj(no_dev_deps_in_source_code)
+    internet_available_mock = mocker.patch(
+        no_dev_deps_in_source_code.__module__ + "." + internet_is_available.__name__,
+        return_value=not internet_is_available(),
+    )
+    unwrapped_func(
+        tmp_path_factory=tmp_path_factory,
+        standard_output_error_template=standard_output_error_template,
+    )
+    internet_available_mock.assert_called_once()
 
 
-def test_package_manager_updated() -> None:
+def test_package_manager_updated(
+    mocker: MockerFixture, standard_output_error_template: str
+) -> None:
     """Test function."""
+    unwrapped_func = unwrapped_obj(package_manager_updated)
+
+    internet_available_mock = mocker.patch(
+        package_manager_updated.__module__ + "." + internet_is_available.__name__,
+        return_value=not internet_is_available(),
+    )
+    unwrapped_func(standard_output_error_template=standard_output_error_template)
+    internet_available_mock.assert_called_once()
+
+    internet_available_mock.return_value = True
+    ci_mock = mocker.patch.object(
+        RemoteVersionController,
+        RemoteVersionController.running_in_ci.__name__,
+        return_value=True,
+    )
+    unwrapped_func(standard_output_error_template=standard_output_error_template)
+    ci_mock.assert_called_once()
