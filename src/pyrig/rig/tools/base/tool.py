a1630dd114930f801b2754463c6b85d7f8e0893e
Winipedia <winipedia@gmx.de>
2026-04-20 01:26:14 +0100

    rm unneeded code section for special cases

--- src/pyrig/rig/tools/base/tool.py
+++ src/pyrig/rig/tools/base/tool.py
@@ -162,9 +162,6 @@ class Tool(DependencySubclass):
         groups: defaultdict[str, list[str]] = defaultdict(list)
         for tool in subclasses:
             t = tool()
-            if not any(t.badge_urls()):
-                # if tuple has empty strings, skip it
-                continue
             groups[t.group()].append(t.badge())
         return groups
 
