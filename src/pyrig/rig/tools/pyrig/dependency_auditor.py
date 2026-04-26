62059f762debf74826354ccae03abb6aa473f316
Winipedia <winipedia@gmx.de>
2026-04-26 04:13:58 +0100

    pyrig sepcific override fix

--- src/pyrig/rig/tools/pyrig/dependency_auditor.py
+++ src/pyrig/rig/tools/pyrig/dependency_auditor.py
@@ -1,20 +1,22 @@
 """Pyrig specific dependency auditor override."""
 
+from pyrig.core.introspection.packages import src_package_is_pyrig
 from pyrig.core.subprocesses import Args
 from pyrig.rig.tools.dependency_auditor import (
     DependencyAuditor as BaseDependencyAuditor,
 )
 
+if src_package_is_pyrig():
 
-class DependencyAuditor(BaseDependencyAuditor):
-    """Pyrig-specific dependency auditor.
+    class DependencyAuditor(BaseDependencyAuditor):
+        """Pyrig-specific dependency auditor.
 
-    Overrides the base DependencyAuditor to apply pyrig-specific overrides.
-    """
-
-    def audit_args(self, *args: str) -> Args:
-        """Build pip-audit command arguments with pyrig-specific overrides.
-
-        Sometimes used to ignore irrelevant vulnerabilities dependencies.
+        Overrides the base DependencyAuditor to apply pyrig-specific overrides.
         """
-        return super().audit_args(*args, "--ignore-vuln", "CVE-2026-3219")
+
+        def audit_args(self, *args: str) -> Args:
+            """Build pip-audit command arguments with pyrig-specific overrides.
+
+            Sometimes used to ignore irrelevant vulnerabilities dependencies.
+            """
+            return super().audit_args(*args, "--ignore-vuln", "CVE-2026-3219")
