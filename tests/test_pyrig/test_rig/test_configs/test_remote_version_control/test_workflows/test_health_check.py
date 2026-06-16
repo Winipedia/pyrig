da05f7e0111d41d5f0f5c56bbc740291fff9650a
Winipedia <winipedia@gmx.de>
2026-06-16 22:18:57 +0100

    no steps in agg job

--- tests/test_pyrig/test_rig/test_configs/test_remote_version_control/test_workflows/test_health_check.py
+++ tests/test_pyrig/test_rig/test_configs/test_remote_version_control/test_workflows/test_health_check.py
@@ -76,7 +76,7 @@ class TestHealthCheckWorkflowConfigFile:
         result = my_test_health_check_workflow().job_health_check()
         assert len(result) == 1, "Expected job to have one key"
         job_name = next(iter(result.keys()))
-        assert "steps" not in result[job_name]
+        assert "steps" in result[job_name]
         assert "needs" in result[job_name]
 
     def test_steps_matrix_health_checks(
