fafba767d9f8ee42629746a922dac2a439c8354e
Winipedia <winipedia@gmx.de>
2026-06-27 17:26:31 +0100

    moved contest and fixture sharing to pyrig-fixtures package

--- tests/test_pyrig/test_rig/test_configs/test_base/test_python.py
+++ tests/test_pyrig/test_rig/test_configs/test_base/test_python.py
@@ -1,11 +1,11 @@
 """module."""
 
+from pyrig_fixtures.rig.configs.conftest import ConftestConfigFile
 from pytest_mock import MockerFixture
 
 from pyrig.rig.configs.base.python import PythonConfigFile
 from pyrig.rig.configs.base.string_ import StringConfigFile
 from pyrig.rig.configs.package_init import PackageInitConfigFile
-from pyrig.rig.configs.project_tester import ProjectTesterConfigFile
 
 
 class TestPythonConfigFile:
@@ -13,29 +13,29 @@ class TestPythonConfigFile:
 
     def test_is_init_file(self) -> None:
         """Test method."""
-        assert not ProjectTesterConfigFile.I.is_init_file()
+        assert not ConftestConfigFile.I.is_init_file()
         assert PackageInitConfigFile.I.is_init_file()
 
     def test__dump(self, mocker: MockerFixture) -> None:
         """Test method."""
-        module_before = ProjectTesterConfigFile.I.module()
+        module_before = ConftestConfigFile.I.module()
         dump_mock = mocker.patch.object(StringConfigFile, "_dump")
-        ProjectTesterConfigFile.I._dump([])  # noqa: SLF001
+        ConftestConfigFile.I._dump([])  # noqa: SLF001
         dump_mock.assert_called_once()
-        module_after = ProjectTesterConfigFile.I.module()
+        module_after = ConftestConfigFile.I.module()
         assert module_before is not module_after
         assert module_before.__name__ == module_after.__name__
         assert module_before.__file__ == module_after.__file__
 
     def test_module(self) -> None:
         """Test method."""
-        assert ProjectTesterConfigFile.I.module().__name__ == "tests.conftest"
+        assert ConftestConfigFile.I.module().__name__ == "tests.conftest"
 
         assert PackageInitConfigFile.I.module().__name__ == "pyrig"
 
     def test_extension(self) -> None:
         """Test method."""
         expected = "py"
-        assert issubclass(ProjectTesterConfigFile, PythonConfigFile)
-        actual = ProjectTesterConfigFile().extension()
+        assert issubclass(ConftestConfigFile, PythonConfigFile)
+        actual = ConftestConfigFile().extension()
         assert actual == expected, f"Expected {expected}, got {actual}"
