a8118538245b347488cefe43f71b76c21ba5da91
Winipedia <winipedia@gmx.de>
2026-07-22 16:53:10 +0100

    moved init file making as a convenience cmd

--- tests/test_pyrig/test_rig/test_cli/test_commands/test_synchronize.py
+++ tests/test_pyrig/test_rig/test_cli/test_commands/test_synchronize.py
@@ -1,7 +1,5 @@
 """test module."""
 
-from pathlib import Path
-
 import pytest
 import typer
 from pytest_mock import MockerFixture
@@ -10,17 +8,12 @@ from pyrig.rig.cli.commands.synchronize import (
     synchronize_project,
 )
 from pyrig.rig.configs.base.config_file import ConfigFile
+from pyrig.rig.configs.pyproject import PyprojectConfigFile
 from pyrig.rig.tests.mirror_test import MirrorTestConfigFile
-from pyrig.rig.tools.programming_language import ProgrammingLanguage
 
 
 def test_synchronize_project(mocker: MockerFixture) -> None:
     """Test function."""
-    make_init_files_mock = mocker.patch.object(
-        ProgrammingLanguage,
-        ProgrammingLanguage.make_init_files.__name__,
-        return_value=(),
-    )
     config_file_mock = mocker.patch.object(
         ConfigFile,
         ConfigFile.validate_concrete_subclasses.__name__,
@@ -29,7 +22,6 @@ def test_synchronize_project(mocker: MockerFixture) -> None:
 
     synchronize_project()
 
-    make_init_files_mock.assert_called_once()
     assert config_file_mock.call_count == len(
         (
             ConfigFile.validate_concrete_subclasses,
@@ -37,7 +29,7 @@ def test_synchronize_project(mocker: MockerFixture) -> None:
         ),
     )
 
-    make_init_files_mock.return_value = (Path(),)
+    config_file_mock.return_value = (PyprojectConfigFile,)
 
     with pytest.raises(typer.Exit):
         synchronize_project()
